cdb5c4ea8d66876d6bf7fe91fb0902268696bf43
kenya-iida <cen315@gmail.com>
2019-12-18 20:32:26 +0900

    ロードシーン用の共通関数作成。

---
+++ ItSayNoDay/Assets/ItSayNoDay/Parts/MySceneManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+
+public static class MySceneManager
+{
+    public static void LoadHomeScene()
+    {
+        SceneManager.LoadScene("Home");
+    }
+
+    public static void LoadCreateRoomScene()
+    {
+        SceneManager.LoadScene("CreateRoom");
+    }
+
+    public static void LoadRulesScene()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+}
